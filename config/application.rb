9db0df60847798257855b7d612953a5ba9ae2014
Jayshree Anandakumar <jayshree@keepworks.com>
2015-09-09 20:26:14 +0530

    Configure twitter gem

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module TwitterStartupBot
   class Application < Rails::Application
+
+    config.autoload_paths += %W(
+      #{config.root}/lib
+      #{config.root}/app/api
+    )
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
